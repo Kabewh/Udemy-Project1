eacfadd4dfaea7c3ea4084ad11ee5ff45900608b
Dani <daniele.viola@continental-corporation.com>
2023-08-07 10:41:50 +0300

    Before big refactor using context

--- reactapp/src/components/BookList.jsx
+++ reactapp/src/components/BookList.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import BookShow from './BookShow'
+import BooksContext from '../context/books'
+
 
 const BookList = ({ books, onDelete, onEdit }) => {
+    const {count, incrementCount} = useContext(BooksContext);
+
     const renderedBooks = books.map((book) => {
         return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
     })
     return (
-        <div className='book-list'>{renderedBooks}</div>
+        <div className='book-list'>
+            {renderedBooks}
+            </div>
     )
 }
 
